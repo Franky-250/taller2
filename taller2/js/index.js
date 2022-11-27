e1339a1e4feaef349efa588aefd43c8e54381e8f
Franky-250 <117626845+Franky-250@users.noreply.github.com>
2022-11-27 17:31:33 -0500

    Add files via upload

---
+++ taller2/js/index.js
@@ -0,0 +1,30 @@
+const prueba = [{
+
+    title: "Típicos Laureles",
+    dire: "Cll 43# 170 - 180",
+    descr:"Los Mejores Tipicos de Medellín",
+
+},{
+    title: "Lo mejor de Italia en Envigado",
+    dire: "Cra 25# 16sur - 80",
+    descr:"Delicias Italianas",
+},
+{   
+    title: "Mar & Mar",
+    dire: "Cra 81# 1sur - 180",
+    descr:"Típicos del Mar",
+
+},
+{
+    title: "Burger",
+    dire: "Cra 25# 10 - 180",
+    descr:"Comida a tú gusto con el mejor sabor"
+}];
+
+export{prueba};
+
+
+
+
+
+
